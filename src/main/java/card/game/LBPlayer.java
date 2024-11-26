08229d235eec2d2ac24c2e3f1205494d07e2b477
Aidan O'Brien <68aobrien@gmail.com>
2024-11-26 13:13:40 -0500

    LBPlayer

---
+++ src/main/java/card/game/LBPlayer.java
@@ -0,0 +1,39 @@
+package card.game;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class LBPlayer {
+    private Socket socket;
+    private Scanner scanner;
+    private PrintWriter writer;
+    private byte shots;
+
+
+
+    public LBPlayer(Socket socket) throws IOException {
+        scanner = new Scanner(socket.getInputStream());
+        writer = new PrintWriter(socket.getOutputStream());
+    }
+
+
+
+    public boolean isAlive() {
+        return shots != 17;
+    }
+
+    public byte getShots() {
+        return shots;
+    }
+
+    public void incrementShots() {
+        shots++;
+    }
+
+    public void kill() {
+        shots = 17;
+        scanner.close();
+    }
+}
--- src/main/java/card/game/LBServer.java
+++ src/main/java/card/game/LBServer.java
@@ -1,33 +1,50 @@
 package card.game;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 import java.util.Scanner;
 import java.net.ServerSocket;
 
 public class LBServer {
     private ServerSocket socket;
     private Scanner scanner;
+    private Random rng;
 
 
     public LBServer(ServerSocket socket) throws IOException {
         this.socket = socket;
+        rng = new Random();
     }
 
+
+
+    private void shuffle(Color[] deck) {
+        for (int i=0; i<19; i++) {
+            int j = rng.nextInt(21 - i);
+            Color temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+        }
+    }
+
+    // private LBPlayer getNextPlayer() {
+
+    // }
 
 
     public static void main(String[] args) throws IOException {
         ServerSocket socket = new ServerSocket(54321);
         LBServer server = new LBServer(socket);
         //Socket[] clients = new Socket[4];
-        Scanner[] scanners = new Scanner[4];
-        PrintWriter[] writers = new PrintWriter[4];
+        LBPlayer[] players = new LBPlayer[4];
         for (int i=0; i<4; i++) {
-            Socket client = socket.accept();
-            scanners[i] = new Scanner(client.getInputStream());
-            writers[i] = new PrintWriter(client.getOutputStream());
+            players[i] = new LBPlayer(socket.accept());
         }
+        Color[] deck = new Color[20];
+        do {
+            
+        } while (true);
     }
 }
